916cedecc042a81e5f0fd11875762cdf2d44bcc5
kheyf <kheyfets03@mail.ru>
2021-09-23 22:23:49 +0300

    wtf

---
+++ cmake-build-debug/CMakeFiles/laba1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laba1.dir/main.c.obj"
+  "laba1.exe"
+  "laba1.exe.manifest"
+  "laba1.pdb"
+  "liblaba1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/laba1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int var;
+    scanf("%d", &var);
+    printf("%d\n", var);
+    char ch;
+    scanf(" %c", &ch);
+    printf("%c", ch);
+    return 0;
+}
